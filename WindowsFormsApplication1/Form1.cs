d923deadd904b7a6a38bb0ebb9cbe982b325cb06
Omar Quimbaya <omar.quimbaya4@gmail.com>
2015-02-09 12:01:05 -0600

    Windows forms and writing to files
    Seeing how windows forms work and experimenting with writing to files
    using windows forms. It's very simple, but this is my first foray into
    anything with Windows Forms. Very interesting stuff!

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (FileStream fstream = File.Create(textBox1.Text))
+                {
+                    Byte[] text = new UTF8Encoding(true).GetBytes(richTextBox1.Text);
+                    fstream.Write(text, 0, text.Length);
+                } 
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.ToString();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader sreader = File.OpenText(textBox2.Text))
+                {
+                    string linestoprint;
+                    while ((linestoprint = sreader.ReadLine()) != null)
+                    {
+                        richTextBox2.Text += linestoprint + "\n";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.ToString();
+            }
+        }
+    }
+}
